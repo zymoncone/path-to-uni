5f2ed5d6fdb2ab22f2fb2e66ffee4428ccc8a5a2
Szymon <szymon@sarnowicz.net>
2023-09-02 22:04:51 -0500

    Added panel, notifications, and table highlights

--- counseling_tool.py
+++ counseling_tool.py
@@ -1,29 +1,71 @@
-from taipy.gui import Gui, notify, navigate
+from taipy.gui import Gui, notify, navigate, Markdown
 import sys
 sys.path.insert(1,'C:/GIT/Hack_The_Classroom/path-to-uni/data')
 import pandas as pd
-from student_data import student_data_df, student_table_dict
+from student_data import student_data_df, student_table_dict_SAT_math, student_table_dict_SAT_rw, student_table_dict_main, student_sig_underperforming, student_mildly_underperforming
 
-student_A_grades = pd.DataFrame({'classes':student_data_df.loc['Student A'][0:5].index,'grades [%]':student_data_df.loc['Student A'][0:5].values})
-
+############### - TAIPY Functions & Properties - ###############
+# Chart Properties
 property_chart_grades = {"type":"bar",
                          "x": "classes",
                          "y": "grades [%]",
                          "title":"Current Grades"}
-
-student_A_SAT = pd.DataFrame({'categories':student_data_df.loc['Student A'][5:7].index,'score':student_data_df.loc['Student A'][5:7].values})
 
 property_chart_SAT = {"type":"bar",
                       "x":"categories",
                       "y":"score",
                       "title":"Predicted Scores"}
 
-# Definition of the page
-student_name = student_data_df.index[0]
-student_table = student_table_dict['Student A']
+# Taipy GUI functions
+def on_button_action(state):
+    navigate(state, to=state.student_name.lower().replace(" ","_"))
+    notify(state, 'info', f'Data updated for: {state.student_name}')
 
+def on_change(state, var_name, var_value):
+    if var_value == False:
+        notify(state, 'info', 'Panel closed, re-open with button')
+    
+def on_user_init(state):
+    notify(state, 'info', f'Data updated for: {state.student_name}')
+    return
+
+def table_style(_1, _2, value):
+    # breakpoint()
+    if value[0] > -1:
+        return "green-cell"
+    elif value[0] > -30:
+        return "yellow-cell"
+    else:
+        return "red-cell"
+
+# Data creation
+student_names = []
+student_grades =[]
+student_SAT = []
+student_tables_SAT_math = []
+student_tables_SAT_rw = []
+student_tables_main = []
+
+for student_name in student_data_df.index:
+
+    student_names.append(student_name)
+
+    student_grades.append(pd.DataFrame({'classes':student_data_df.loc[student_name][0:5].index,'grades [%]':student_data_df.loc[student_name][0:5].values}))
+
+    student_SAT.append(pd.DataFrame({'categories':student_data_df.loc[student_name][5:7].index,'score':student_data_df.loc[student_name][5:7].values}))
+
+    student_tables_SAT_math.append(student_table_dict_SAT_math[student_name])
+
+    student_tables_SAT_rw.append(student_table_dict_SAT_rw[student_name])
+
+    student_tables_main.append(student_table_dict_main[student_name])
+
+########################################## - PAGES - ##########################################
+# ISSUES WITH TRYING TO CREATE FOR LOOP FOR EACH PAGE, SO WROTE OUT INDIVIDUALLY FOR NOW
+show_pane = True
+student_name = student_names[0]
 page_1 = """
-<|{student_name}|id=student_title|>
+<|{student_names[0]}|id=student_title|>
 
 <|layout|columns=1 1|id=search|
 
@@ -35,35 +77,179 @@ page_1 = """
 
 <|layout|columns=1 1|
 
-<|{student_A_grades}|chart|properties={property_chart_grades}|width=100%|>
+<|{student_grades[0]}|chart|properties={property_chart_grades}|width=100%|>
 
-<|{student_A_SAT}|chart|properties={property_chart_SAT}|width=100%|>
+<|{student_SAT[0]}|chart|properties={property_chart_SAT}|width=100%|>
 
 |>
 
-<|Favorited Universities|id=table_title|>
-<|{student_table}|table|id=my_table|>
+<|Favorited Universities (+/- compared to {student_names[0]})|id=table_title|>
+
+<|layout|columns=1 1 1|
+
+<|{student_tables_main[0]}|table|show_all|id=my_table|>
+
+<|{student_tables_SAT_math[0]}|table|style=table_style|show_all|id=my_table|>
+
+<|{student_tables_SAT_rw[0]}|table|style=table_style|show_all|id=my_table|>
+
+|>
+
+<|d-flex|
+<|{show_pane}|pane|persistent|width=400px|
+##### Student Status
+<|{student_sig_underperforming}|table|show_all|>
+
+<|{student_mildly_underperforming}|table|show_all|>
+|>
+<|Open Panel|button|on_action={lambda s: s.assign("show_pane", True)}|>
+|>
 """
 
+student_name = student_names[1]
 page_2 = """
-    TO BE FILLED IN
+<|{student_names[1]}|id=student_title|>
+
+<|layout|columns=1 1|id=search|
+
+<|{student_name}|input|>
+
+<|Go To|button|on_action=on_button_action|>
+
+|>
+
+<|layout|columns=1 1|
+
+<|{student_grades[1]}|chart|properties={property_chart_grades}|width=100%|>
+
+<|{student_SAT[1]}|chart|properties={property_chart_SAT}|width=100%|>
+
+|>
+
+<|Favorited Universities (+/- compared to {student_names[0]})|id=table_title|>
+
+<|layout|columns=1 1 1|
+
+<|{student_tables_main[1]}|table|show_all|id=my_table|>
+
+<|{student_tables_SAT_math[1]}|table|style=table_style|show_all|id=my_table|>
+
+<|{student_tables_SAT_rw[1]}|table|style=table_style|show_all|id=my_table|>
+
+|>
+
+<|d-flex|
+<|{show_pane}|pane|persistent|width=400px|
+##### Student Status
+<|{student_sig_underperforming}|table|show_all|>
+
+<|{student_mildly_underperforming}|table|show_all|>
+|>
+<|Open Panel|button|on_action={lambda s: s.assign("show_pane", True)}|>
+|>
 """
 
-def on_button_action(state):
-    navigate(state, to=state.student_name.lower().replace(" ","_"))
-    notify(state, 'info', f'Data updated for: {state.student_name}')
+student_name = student_names[2]
+page_3 = """
+<|{student_names[2]}|id=student_title|>
 
-def on_change(state, var_name, var_value):
-    notify(state, 'info', f'{state.student_name} needs support')
-    
-def on_user_init(state):
-    notify(state, 'info', f'Data updated for: {state.student_name}')
-    return
-    
-pages = {"/":"<|toggle|theme|>\n<center>\n<|navbar|>\n</center>",
-        "student_a":page_1,
-        "student_b":page_2,
-        "student_c":page_2,
-        "student_d":page_2}
+<|layout|columns=1 1|id=search|
 
-Gui(pages=pages,css_file="counseling_tool.css").run(use_reloader=True) # use_reloader=True for dev mode / reloads on save
+<|{student_name}|input|>
+
+<|Go To|button|on_action=on_button_action|>
+
+|>
+
+<|layout|columns=1 1|
+
+<|{student_grades[2]}|chart|properties={property_chart_grades}|width=100%|>
+
+<|{student_SAT[2]}|chart|properties={property_chart_SAT}|width=100%|>
+
+|>
+
+<|Favorited Universities (+/- compared to {student_names[0]})|id=table_title|>
+
+<|layout|columns=1 1 1|
+
+<|{student_tables_main[2]}|table|show_all|id=my_table|>
+
+<|{student_tables_SAT_math[2]}|table|style=table_style|show_all|id=my_table|>
+
+<|{student_tables_SAT_rw[2]}|table|style=table_style|show_all|id=my_table|>
+
+|>
+
+<|d-flex|
+<|{show_pane}|pane|persistent|width=400px|
+##### Student Status
+<|{student_sig_underperforming}|table|show_all|>
+
+<|{student_mildly_underperforming}|table|show_all|>
+|>
+<|Open Panel|button|on_action={lambda s: s.assign("show_pane", True)}|>
+|>
+"""
+
+student_name = student_names[3]
+page_4 = """
+<|{student_names[3]}|id=student_title|>
+
+<|layout|columns=1 1|id=search|
+
+<|{student_name}|input|>
+
+<|Go To|button|on_action=on_button_action|>
+
+|>
+
+<|layout|columns=1 1|
+
+<|{student_grades[3]}|chart|properties={property_chart_grades}|width=100%|>
+
+<|{student_SAT[3]}|chart|properties={property_chart_SAT}|width=100%|>
+
+|>
+
+<|Favorited Universities (+/- compared to {student_names[0]})|id=table_title|>
+
+<|layout|columns=1 1 1|
+
+<|{student_tables_main[3]}|table|show_all|id=my_table|>
+
+<|{student_tables_SAT_math[3]}|table|style=table_style|show_all|id=my_table|>
+
+<|{student_tables_SAT_rw[3]}|table|style=table_style|show_all|id=my_table|>
+
+|>
+
+<|d-flex|
+<|{show_pane}|pane|persistent|width=400px|
+##### Student Status
+<|{student_sig_underperforming}|table|show_all|>
+
+<|{student_mildly_underperforming}|table|show_all|>
+|>
+<|Open Panel|button|on_action={lambda s: s.assign("show_pane", True)}|>
+|>
+"""
+
+# root page
+root_md = """
+<|OPEN ADVICE|id=title|>
+
+<|toggle|theme|>\n<center>\n<|navbar|>\n</center>
+"""
+
+######################### - RUN APP - #########################
+# create pages
+pages = {"/":root_md,
+         "student_a":page_1,
+         "student_b":page_2,
+         "student_c":page_3,
+         "student_d":page_4}
+
+# run GUI
+app = Gui(pages=pages,css_file="counseling_tool.css")
+app.run(use_reloader=True) # use_reloader=True for dev mode / reloads on save
--- data/student_data.py
+++ data/student_data.py
@@ -26,7 +26,10 @@ def process_data(data=student_data) -> pd.DataFrame:
 # create student tables from data
 def create_student_tables(df, compare_75th_percentile=True):
 
-    student_table_dict = {}
+    student_table_dict_SAT = {}
+    student_table_dict_SAT_math = {}
+    student_table_dict_SAT_rw = {}
+    student_table_dict_main = {}
 
     if compare_75th_percentile:
         read_col = "SAT_Reading_75th_p" 
@@ -41,21 +44,78 @@ def create_student_tables(df, compare_75th_percentile=True):
 
         SAT_Math_diff = [df.loc[student_name]['SAT Math'] - college_admis_df.loc[school_name][math_col] for school_name in favorites]
         SAT_RW_diff = [df.loc[student_name]['SAT Reading-Writing'] - college_admis_df.loc[school_name][read_col] for school_name in favorites]
-        GPA = np.mean(df.loc[student_name][0:5]) / 100 * 4.0
-        GPA_diff = ["N/A" for i in range(len(SAT_RW_diff))] #[np.round(GPA - college_admis_df.loc[school_name].GPA,2) for school_name in favorites]
+        GPA = np.round(np.mean(df.loc[student_name][0:5]) / 100 * 4.0, 2)
+        # Temporary until GPA data is available
+        GPA_diff = [np.round(random.uniform(-0.5,0.5),2) for i in range(len(SAT_RW_diff))]
 
-        student_table = {"school names":favorites, 
-                           "GPA":GPA_diff, 
-                           "SAT Math":SAT_Math_diff,
-                           "SAT Reading + Writing":SAT_RW_diff}
+        student_table_main = {"school names":favorites,
+                              f"GPA [{GPA} Overall]":GPA_diff}
         
-        student_table_dict[student_name] = student_table
+        student_table_SAT = {"SAT Math":SAT_Math_diff,
+                             "SAT Reading + Writing":SAT_RW_diff}
+        
+        student_table_dict_SAT[student_name] = student_table_SAT
+        student_table_dict_SAT_math[student_name] = {"SAT Math":SAT_Math_diff}
+        student_table_dict_SAT_rw[student_name] = {"SAT Reading + Writing":SAT_RW_diff}
+        student_table_dict_main[student_name] = student_table_main
 
-    return student_table_dict
+    return student_table_dict_SAT, student_table_dict_SAT_math, student_table_dict_SAT_rw, student_table_dict_main
+
+# return significantly under performing students
+def check_sig_underperforming_students(table,limit=-70):
+    student_underperforming = []
+    for student_name, student_dict in table.items():
+        # breakpoint()
+        Math_list = student_dict['SAT Math']
+        RW_list = student_dict['SAT Reading + Writing']
+        print('Math List: ', Math_list)
+
+        res_Math = True in (ele < limit for ele in Math_list)
+        res_RW = True in (ele < limit for ele in RW_list)
+
+        if res_Math and res_RW:
+            text = f'> {student_name} needs help in Math & English'
+        elif res_Math:
+            text = f'> {student_name} needs help in Math'
+        elif res_RW:
+            text = f'> {student_name} needs help in English'
+        else:
+            text = None
+
+        if text != None:
+            student_underperforming.append(text)
+    
+    return pd.DataFrame({'Students Of Concern':student_underperforming})
+   
+# return mildly under performing students
+def check_mildly_performing_students(table,l_bound=-50,r_bound=-10):
+    student_underperforming = []
+    for student_name, student_dict in table.items():
+        # breakpoint()
+        Math_list = student_dict['SAT Math']
+        RW_list = student_dict['SAT Reading + Writing']
+
+        res_Math = True in (l_bound < ele < r_bound for ele in Math_list)
+        res_RW = True in (l_bound < ele < r_bound for ele in RW_list)
+
+        if res_Math and res_RW:
+            text = f'> {student_name} is so close in reaching their goal with Math & English'
+        elif res_Math:
+            text = f'> {student_name} is so close in reaching their goal with Math'
+        elif res_RW:
+            text = f'> {student_name} is so close in reaching their goal with English'
+        else:
+            text = None
+
+        if text != None:
+            student_underperforming.append(text)
+    
+    return pd.DataFrame({'Students To Watch':student_underperforming})
 
 ################################################# - MAIN - #################################################
 student_data_df = process_data()
-student_table_dict = create_student_tables(student_data_df)
-
+student_table_dict_SAT, student_table_dict_SAT_math, student_table_dict_SAT_rw, student_table_dict_main = create_student_tables(student_data_df)
+student_sig_underperforming = check_sig_underperforming_students(student_table_dict_SAT)
+student_mildly_underperforming = check_mildly_performing_students(student_table_dict_SAT)
 
 # breakpoint()
